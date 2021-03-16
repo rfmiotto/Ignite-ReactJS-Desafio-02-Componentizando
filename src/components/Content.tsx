8076368818f680d4e2087161c8f9c720f6cae371
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2021-03-16 09:09:08 -0300

    Finish challenge - Using custom hook n context

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
+import { useMovies } from '../hooks/useMovies'
+
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
 export function Content() {
-  // Complete aqui
+  const { movies, selectedGenre } = useMovies();
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
